7f954fcaaa49e8ae6b1084ee180b97d52835c003
abc <abc@gmail.com>
2024-02-01 18:01:41 +0530

    Add project files.

---
+++ LayoutAssesment/Controllers/AssessmentController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LayoutAssesment.Controllers
+{
+    public class AssessmentController : Controller
+    {
+        // GET: Assessment
+        public ActionResult Index()
+        {
+            List<String> Names = new List<String>
+            {
+                "Preetham",
+                "Praneeth",
+                "Sreenidhi"
+            };
+            ViewBag.msg = " Welcome! Here are our valued customers:";
+            ViewBag.CNames = Names;
+            return View();
+        }
+        public ActionResult RedirectDemo()
+        {
+            TempData["message"] = "Welcome";
+            return RedirectToAction("DisplayMessage");
+        }
+        public ActionResult DisplayMessage()
+        {
+            var message = TempData["message"] as string;
+            ViewBag.message1 = message;
+            TempData.Clear();
+            return View();
+        }
+
+
+
+
+
+    }
+}
